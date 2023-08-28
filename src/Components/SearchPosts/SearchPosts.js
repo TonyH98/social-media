62411fcf9ca0a429275c152bc834bc58f44fb435
TonyH98 <tonyhoang@pursuit.org>
2023-08-28 13:21:42 -0400

    Able to get posts by hashtags

--- src/Components/SearchPosts/SearchPosts.js
+++ src/Components/SearchPosts/SearchPosts.js
@@ -1,7 +1,25 @@
-
-
+import { useDispatch , useSelector } from "react-redux";
+import { getSearchPost } from "../../Store/userActions";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
 
 function SearchPosts(){
+
+const {tag_name} = useParams()
+
+
+const dispatch = useDispatch();
+
+const getPosts = useSelector((state) => state.get_search.searchPost);
+
+
+useEffect(() => {
+dispatch(getSearchPost(tag_name))
+}, [dispatch])
+
+
+console.log(getPosts)
+
     return(
         <div>Hello</div>
     )

f235164cdc9209b23cdd41eaccfe6de3987fc305
TonyH98 <tonyhoang@pursuit.org>
2023-09-29 18:58:56 -0400

    Option to get all the search from posts and replies

--- src/Components/SearchPosts/SearchPosts.js
+++ src/Components/SearchPosts/SearchPosts.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { useEffect , useState } from "react";
 import TagPosts from "./tagPosts";
 import TagReplies from "./tagReplies";
+import AllSearch from "./AllSearch";
 import "./SearchPosts.css"
 
 function SearchPosts(){
@@ -12,7 +13,7 @@ const {tag_name} = useParams()
 
 let [option , setOption] = useState(0)
 let options = ["Posts", "Replies", "All"]
-
+let [allSearch , setAllSearch] = useState([])
 const dispatch = useDispatch();
 
 const getPosts = useSelector((state) => state.get_search.searchPost);
@@ -22,6 +23,7 @@ const getReplies = useSelector((state) => state.search2.search)
 useEffect(() => {
 dispatch(getSearchPost(tag_name))
 dispatch(getSearchReplies(tag_name))
+setAllSearch([...getPosts , ...getReplies])
 }, [dispatch])
 
 
@@ -52,6 +54,19 @@ function optionContent(selected) {
         </div>
       );
     }
+  if(selected === 2){
+    return (
+      <div className="search_post_container">
+        {allSearch.map((tag) => {
+          return (
+            <div  className="posts-border-container">
+              <AllSearch tag={tag}/>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
     
   }
 

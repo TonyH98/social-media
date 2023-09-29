fd7c13720fa539304b405f2411375b56cb2ace56
TonyH98 <tonyhoang@pursuit.org>
2023-09-29 13:08:07 -0400

    Display the posts with the search tags

--- src/Components/SearchPosts/SearchPosts.js
+++ src/Components/SearchPosts/SearchPosts.js
@@ -1,12 +1,16 @@
 import { useDispatch , useSelector } from "react-redux";
 import { getSearchPost } from "../../Store/userActions";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
+import TagPosts from "./tagPosts";
+import "./SearchPosts.css"
 
 function SearchPosts(){
 
 const {tag_name} = useParams()
 
+let [option , setOption] = useState(0)
+let options = ["Posts", "Replies", "All"]
 
 const dispatch = useDispatch();
 
@@ -18,10 +22,45 @@ dispatch(getSearchPost(tag_name))
 }, [dispatch])
 
 
+function optionContent(selected) {
+    if (selected === 0) {
+      return (
+        <div>
+          {getPosts.map((tag) => {
+            return (
+              <div  className="posts-border-container">
+                <TagPosts tag={tag}/>
+              </div>
+            );
+          })}
+        </div>
+      );
+    }
+    
+    
+  }
+
+
 
 
     return(
-        <div>Hello</div>
+      <div className="search_post_page">
+        <div className="search_post_first_section">
+            <div className="tag_name_container">
+                <h1>{getPosts[0]?.tag_names}</h1>
+            </div>
+            <div className="search_option_button">
+            {options.map((opt , index) => {
+                        return(
+                    <button onClick={() => setOption(index)} className={index === option ? `active options` : 'options'} key={index}>{opt}</button>
+                        )
+                    })}
+            </div>
+        </div>
+    <div className="search_post_second_section">
+        {optionContent(option)}
+    </div>
+      </div>
     )
 }
 
---
+++ src/Components/SearchPosts/tagPosts.js
@@ -0,0 +1,77 @@
+
+
+function TagPosts({tag}){
+
+    function highlightMentions(content) {
+        const mentionPattern = /@(\w+)/g;
+        const hashtagPattern = /#(\w+)(?=\W|$)/g;
+        
+        const highlightedContent = content
+        .replace(mentionPattern, '<span class="mention">$&</span>')
+        .replace(hashtagPattern, `<span class="hashtag" style="color: blue;">$&</span>`)
+        return <div dangerouslySetInnerHTML={{ __html: highlightedContent }} />;
+    }
+
+    function formatDate(inputDate){
+        const months = [
+            "Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+          ];
+    
+        const [month, day, year] = inputDate.split("/").map(Number);
+        const formattedMonth = months[month - 1]
+        const formattedYear = year.toString()
+    
+        return `${formattedMonth} ${day}, ${formattedYear}`
+    }
+
+    console.log(tag)
+    
+    return(
+        <div className="posts_content">
+
+        <div className="posts_extra_container">
+
+        <div className="post_user_profile_container">
+        <img
+        src={tag.creator_details.profile_img}
+        alt={tag.creator_details.profile_img}
+        className="post_user_profile"
+        />
+        </div>
+
+    <div className="post_user_info_date_container">
+
+    <div className="post_user_profile">
+
+    {tag.creator_details.profile_name} | @{tag.creator_details.username} | {formatDate(tag.posts_details?.date_created)}
+
+    </div>
+
+        
+    <div className="posts_content_text_container">
+
+        <div className="post_text">
+
+           {highlightMentions(tag.posts_details.content)}
+        </div>
+         <div className="posts_img_container">
+        {tag.post_details?.image === "null" ? null : (
+
+            <img src={tag.posts_details?.image} alt={tag.posts_details?.image} className="posts_img"/>
+        )}
+
+        </div> 
+    
+     </div>
+
+     
+     </div>
+
+        </div>
+        
+     </div>
+    )
+}
+
+export default TagPosts

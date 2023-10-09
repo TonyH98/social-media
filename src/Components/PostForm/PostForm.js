81ffe9d697262662cd2a2f372319c9ea45938237
TonyH98 <tonyhoang@pursuit.org>
2023-10-09 13:55:01 -0400

    Able to filter @ users when making a post

--- src/Components/PostForm/PostForm.js
+++ src/Components/PostForm/PostForm.js
@@ -1,15 +1,20 @@
 import "./Post.css"
 
-import { useState , useEffect} from 'react';
-import { useNavigate, Link } from 'react-router-dom';
-import axios from 'axios';
+import { useState , useEffect, useRef} from 'react';
 import { useDispatch } from 'react-redux';
 import { createPost } from "../../Store/userActions";
+import axios from "axios";
 
 const API = process.env.REACT_APP_API_URL;
 function PostForm ({open, onClose, users, plan}){
 
   const dispatch = useDispatch();
+
+    let [otherUsers , setOtherUsers] = useState([])
+
+    let [mentionUsers , setMentionUsers] = useState([])
+
+    const textareaRef = useRef(null);
 
 
     let [posts, setPosts] = useState({
@@ -29,7 +34,14 @@ function PostForm ({open, onClose, users, plan}){
         }
     }, [users?.id]);
     
-    
+    useEffect(() => {
+      axios.get(`${API}/users`)
+      .then((res) => {
+        setOtherUsers(res.data)
+      })
+    }, [API])
+  
+
     const handleTextChange = (event) => {
         if(event.target.id === "posts_img"){
             const file = event.target.files[0];
@@ -66,11 +78,32 @@ function PostForm ({open, onClose, users, plan}){
             else{
               event.target.value = value.substr(0,250)
             }
+
+            const mentionMatches = value.match(/@(\w+)/g);
+
+            if (mentionMatches) {
+              const mentions = mentionMatches.map((mention) => mention.substring(1));
+              const filteredUsers = otherUsers.filter((user) =>
+                mentions.some((mention) => user.username.toLowerCase().includes(mention.toLowerCase()))
+              );
+              setMentionUsers(filteredUsers);
+            } else {
+              setMentionUsers([]);
+            }
+
           }
         }
     };
     
-
+   
+    const handleMention = (user) => {
+   
+      const newContent = `@${user.username}`
+    
+      setPosts({ ...posts, content: newContent });
+    
+      setMentionUsers([]); // Clear mention suggestions
+    };
 
       const handleSubmit = (event) => {
         event.preventDefault();
@@ -101,7 +134,7 @@ function PostForm ({open, onClose, users, plan}){
 
     
     if(!open) return null
-    console.log(plan)
+  
     
     return(
         <div className="overlay">
@@ -119,11 +152,12 @@ function PostForm ({open, onClose, users, plan}){
   
   <label htmlFor="content" className='label-signup'>Post:
   <textarea
-    id="content"
-    required
-    value={posts.content}
-    onChange={handleTextChange}
-  />
+  id="content"
+  required
+  value={posts.content}
+  onChange={handleTextChange}
+  ref={textareaRef} // Add this line
+/>
   
   <p className={`${plan?.images ? 
     (posts?.content.length >= 400 ? 'text-red-700' : null) 
@@ -131,6 +165,15 @@ function PostForm ({open, onClose, users, plan}){
   {posts?.content.length} / {plan?.images ? 400 : 250} characters
 </p>
 
+{mentionUsers.length > 0 && (
+    <ul className="mention-suggestions">
+      {mentionUsers.map((user) => (
+        <li key={user.id} onClick={() => handleMention(user)}>
+          @{user.username}
+        </li>
+      ))}
+    </ul>
+  )}
   </label>
 
   <label htmlFor="posts_img" className='label-signup'>
